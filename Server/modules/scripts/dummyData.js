382f6a162338246d9456db688c1fb06d865452d8
thelias <eliastheviking@gmail.com>
2015-12-06 12:48:08 -0800

    fixed the dummy data script, so it has parameters

--- Server/modules/scripts/dummyData.js
+++ Server/modules/scripts/dummyData.js
@@ -8,15 +8,28 @@ var connectionOptions = {}
 //mongoose.set('debug', true);
 var db = mongoose.connect(connString, connectionOptions);
 // When successfully connected
+User = mongoose.model('User');
 mongoose.connection.on('connected', function () {
     logger.info('Mongoose Connected', {time: new Date(), code: 'M3'});
 });
+
+var user1 = process.argv[2]
+var user2 = process.argv[3]
+var testUsers = []
+User.findOne({name: user1}, function(err, user){
+    if(err) return err;
+     testUsers.push(user._id);
+})
+User.findOne({name: user2}, function(err, user){
+    if(err) return err;
+    testUsers.push(user._id);
+})
 for (var i = 0; i < 20; i++) {
     conversationController.addConversation({
         name: 'Test convo',
         messages: [{text: 'test at test at test', name: 'elias'}, {text: 'test at test t', name: 'ian'}],
         messageCount: 2,
-        users: ['5663c3d5649d120021a78fd8', '5663cc9a162371ec2f084e27']
+        users: testUsers
     }, function (err, item) {
         if (err) return err;
         console.log("added convo")
